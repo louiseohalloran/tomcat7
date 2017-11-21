117ab205e922111a7e68241deeeb887b1b0b39b0
louiseohalloran <louise.ohalloran@ie.ibm.com>
2017-11-21 09:55:02 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ variable "softlayer_username" {
   description = "Your IBM Cloud Infrastructure (SoftLayer) user name."
 }
 variable "softlayer_api_key" {
-  description = "Your IBM Cloud Infrastructure (SoftLayer) API key.”
+  description = "Your IBM Cloud Infrastructure (SoftLayer) API key."
 }
 
 provider "ibm" {
